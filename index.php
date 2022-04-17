ec10d997e44a493d99f9340b9fd6fd180225a311
Madhuja Sinha <87406372+Madhuja01@users.noreply.github.com>
2022-04-17 18:29:24 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -103,6 +103,71 @@ $row4=mysqli_num_rows($run4);
         </a>
       </div>
     </div>
+    
+    <div class="main-div">
+    <div class="head">
+      <p class="head-para"><i class="fas fa-book"></i>&ensp;ISSUED LIST</p>
+    </div>
+    
+    <hr class="h1" />
+    
+    <div class="div-content">
+      <div>
+        <form action="#" method="post">
+          <p class="font">Show:&ensp;
+          <select class="entries" id="entries" name="entries">
+            <option value="" disabled selected hidden>5</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>&ensp;entries</p>
+        </form>
+      </div>
+      
+      <div>
+        <p class="font">Search:&ensp;<input type="text" id="my_input" name="search" placeholder="Search here" onkeyup="searching();" /></p>
+      </div>
+    </div>
+    
+    <table class="my-table" id="my-table" border="0">
+      <thead>
+        <tr>
+          <th style="width: 42px;">S.No.</th>
+          <th>Admission No.</th>
+          <th>Student Name</th>
+          <th>Book Name</th>
+          <th>Book Code</th>
+          <th style="width: 85px;">Issue Date</th>
+        </tr>
+      </thead>
+        
+      <tbody>
+        <?php
+            $sno=0;
+		    $sql = "SELECT student.studentname,book.bookname,issue.admission_no,issue.bookcode,issue.doc FROM `student`,`book`,`issue` WHERE issue.admission_no=student.admission_no AND issue.bookcode=book.bookcode AND issue.status=1";
+		    $run = mysqli_query($con, $sql);
+		    while($row = mysqli_fetch_assoc($run))
+		    {
+				$sno = $sno+1;
+				$datestore = $row['doc'];
+    			$newvar = new DateTime($datestore);
+	    		$ndate = $newvar->format('d-m-Y');
+				
+  	  		    echo '<tr>';
+	  		      echo '<td>'.$sno.'</td>';
+				  echo '<td>'.$row['admission_no'].'</td>';
+    		      echo '<td>'.$row['studentname'].'</td>';
+    		      echo '<td>'.$row['bookname'].'</td>';
+				  echo '<td>'.$row['bookcode'].'</td>';
+				  echo '<td>'.$ndate.'</td>';
+		   	    echo '</tr>'; 
+		    }
+	    ?>
+      </tbody>
+    </table>
+  </div>
 </div>
 
 <script type="text/javascript">
@@ -126,6 +191,32 @@ window.onclick = function(event) {
 	if(event.target == modal) {
 		modal.style.display = "none";
 	}
+}
+</script>
+
+<script type="text/javascript">
+function searching() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("my_input");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("my-table");
+  tr = table.getElementsByTagName("tr");
+  for (i=0; i<tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    td1 = tr[i].getElementsByTagName("td")[2];
+	td2 = tr[i].getElementsByTagName("td")[3];
+    td3 = tr[i].getElementsByTagName("td")[4];
+	td4 = tr[i].getElementsByTagName("td")[5];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if ((txtValue.toUpperCase().indexOf(filter) > -1) || (td1.innerHTML.toUpperCase().indexOf(filter) > -1) || (td2.innerHTML.toUpperCase().indexOf(filter) > -1) || (td3.innerHTML.toUpperCase().indexOf(filter) > -1) || (td4.innerHTML.toUpperCase().indexOf(filter) > -1)) {
+        tr[i].style.display = "";
+      } 
+	  else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
 }
 </script>
 </body>
